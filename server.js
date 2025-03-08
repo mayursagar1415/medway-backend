68ecc755590f1e6f37070f61912e5f51b769ceca
mayursagar1415 <mayursagar1415@gmail.com>
2025-03-08 09:27:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -118,5 +118,5 @@ app.put("/api/doctors/:id", async (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+//const PORT
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
